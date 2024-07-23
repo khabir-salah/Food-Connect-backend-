e510bef6d4968782385fe04918f4596f4a6bc692
Salahudeen Abdulkabir <sabdulkabir19@gmail.com>
2024-07-23 09:22:57 +0100

    bug fixed

---
+++ Api/Controllers/DonationController.cs
@@ -0,0 +1,47 @@
+﻿using Application.Features.Command.Delete;
+using Application.Features.DTOs;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/Donation")]
+    [ApiController]
+    public class DonationController : ControllerBase
+    {
+        private readonly IMediator mediator;
+        public DonationController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpPost("Create-Donation")]
+        public async Task<IActionResult> CreateDonation(CreateDonationCommandModel.CreateDonationCommand request)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var donation = mediator.Send(request);
+            if(!donation.IsCompletedSuccessfully)
+            {
+                return BadRequest(ModelState);
+            }
+            return Ok(donation);
+        }
+
+
+        [HttpPost("Delete-Donation")]
+        public async Task<IActionResult> DeleteDonation(DeleteDonation.DeleteDonationCommand request)
+        {
+            var delete = mediator.Send(request);
+            if (!delete.IsCompletedSuccessfully)
+            {
+                return BadRequest(ModelState);
+            }
+            return Ok(delete);
+        }
+
+    }
+}
--- Api/Controllers/IdentityController.cs
+++ Api/Controllers/IdentityController.cs
@@ -1,18 +1,11 @@
-﻿using Application.Command;
-using Application.Interfaces;
-using Application.Queries;
+﻿using Application.Features.Command.Create;
+using Application.Features.DTOs;
+using Application.Features.Interfaces.IServices;
+using Application.Features.Queries.GeneralServices;
 using Asp.Versioning;
-using Domain.Entities;
-using Infrastructure.persistence.Repository.Implementation;
 using MediatR;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
-using static Application.Queries.ResetPasswordService;
-using static Application.Queries.UserLogin;
-using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
 
 namespace Api.Controllers
 {
@@ -22,25 +15,23 @@ namespace Api.Controllers
     public class IdentityController : ControllerBase
     {
         private readonly IMediator _mediator;
-        private readonly UserManager<User> _userManager;
-        private readonly Authentication _authentication;
-        private readonly EmailService _emailService;
-        private readonly ResetPasswordService _resetPasswordService;
-        private readonly IUserRepository _userRepo;
+        private readonly IAuthentication _authentication;
+        private readonly IEmailService _emailService;
+        private readonly ICurrentUser _user;
+        private readonly ITokenService _tokenService;
 
-        public IdentityController(IMediator mediator, Authentication authentication, UserManager<User> userManager, EmailService emailService, ResetPasswordService resetPasswordService, IUserRepository userRepo)
+        public IdentityController(IMediator mediator, IAuthentication authentication, IEmailService emailService, ICurrentUser user, ITokenService tokenService)
         {
             _mediator = mediator;
             _authentication = authentication;
-            _userManager = userManager;
             _emailService = emailService;
-            _resetPasswordService = resetPasswordService;
-            _userRepo = userRepo;
+            _user = user;
+            _tokenService = tokenService;
         }
 
 
-        [HttpPost("RegisterManager")]
-        public async Task<IActionResult> CreateManager(CreateManager.ManagerRequestModel request)
+        [HttpPost("Register-Manager")]
+        public async Task<IActionResult> CreateManager(CreateManagerCommandModel.CreateManagerCommand request)
         {
             if (!ModelState.IsValid)
             {
@@ -51,8 +42,8 @@ namespace Api.Controllers
             {
                 return BadRequest(request);
             }
-            var user = await _userManager.FindByIdAsync(response.Data.UserId.ToString());
-            var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+            var user = await _user.GetUserAsync(response.Data.UserId);
+            var token = await _tokenService.GenerateEmailConfirmationToken(response.Data.UserId);
             var callbackUrl = Url.Action("ConfirmEmail", "Identity", new { userId = user.Id, token }, protocol: HttpContext.Request.Scheme);
             await _emailService.SendEmailConfirmationAsync(user.Email, callbackUrl);
 
@@ -60,8 +51,8 @@ namespace Api.Controllers
         }
 
 
-        [HttpPost("RegisterFamily")]
-        public async Task<IActionResult> CreateFamily(CreateFamily.FamilyRequestModel request)
+        [HttpPost("Register-Family")]
+        public async Task<IActionResult> CreateFamily(CreateFamilyCommandModel.CreateFamilyCommand request)
         {
             if (!ModelState.IsValid)
             {
@@ -72,16 +63,17 @@ namespace Api.Controllers
             {
                 return BadRequest(request);
             }
-            var user = await _userManager.FindByIdAsync(response.Data.UserId.ToString());
-            var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+            var user = await _user.GetUserAsync(response.Data.UserId);
+            var token = await _tokenService.GenerateEmailConfirmationToken(response.Data.UserId);
             var callbackUrl = Url.Action("ConfirmEmail", "Identity", new { userId = user.Id, token }, protocol: HttpContext.Request.Scheme);
-            await _emailService.SendEmailConfirmationAsync (user.Email, callbackUrl);
+            await _emailService.SendEmailConfirmationAsync(user.Email, callbackUrl);
+
             return Ok(new { Message = "Registration successful. Please check your email to confirm your account." });
         }
 
 
 
-        [HttpPost("RegisterOraganization")]
+        [HttpPost("Register-Oraganization")]
         public async Task<IActionResult> CreateOrganization(CreateOrganization.OraganizationRequestModel request)
         {
             if (!ModelState.IsValid)
@@ -94,37 +86,47 @@ namespace Api.Controllers
                 return BadRequest(request);
             }
 
-            var user = await _userManager.FindByIdAsync(response.Data.UserId.ToString());
-            var token = await _userManager.GenerateEmailConfirmationTokenAsync (user);
-            var callbackUrl = Url.Action("ConfirmEmail", "Identity", new { userId = user.Id, token }, protocol:
-                HttpContext.Request.Scheme);
+            var user = await _user.GetUserAsync(response.Data.UserId);
+            var token = await _tokenService.GenerateEmailConfirmationToken(response.Data.UserId);
+            var callbackUrl = Url.Action("ConfirmEmail", "Identity", new { userId = user.Id, token }, protocol: HttpContext.Request.Scheme);
             await _emailService.SendEmailConfirmationAsync(user.Email, callbackUrl);
+
             return Ok(new { Message = "Registration successful. Please check your email to confirm your account." });
         }
 
 
-        [HttpPost("RegisterRecipient")]
-        public async Task<IActionResult> CreateRecipient(CreateRecipient.RecipientRequestModel request)
+        [HttpPost("Register-Recipient")]
+        public async Task<IActionResult> CreateRecipient(CreateRecipientCommandModel.CreateRecipientCommand request)
         {
             if (!ModelState.IsValid)
             {
-                return BadRequest(request);
+                return BadRequest("All field must be completed");
             }
             var response = await _mediator.Send(request);
             if (!response.IsSuccessfull)
             {
-                return BadRequest(request);
+                return BadRequest(response.Message);
             }
-            var user = await _userManager.FindByIdAsync(response.Data.UserId.ToString());
-            var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-            var callbackUrl = Url.Action("ConfirmEmail", "Identity", new { userId = user.Id, token }, protocol : HttpContext.Request.Scheme);
-            await _emailService.SendEmailConfirmationAsync (user.Email, callbackUrl);
+            var user = await _user.GetUserAsync(response.Data.UserId);
+            var token =  await _tokenService.GenerateEmailConfirmationToken(response.Data.UserId);
+            var callbackUrl = Url.Action("ConfirmEmail", "Identity", new { userId = user.Id, token }, protocol: HttpContext.Request.Scheme);
+            await _emailService.SendEmailConfirmationAsync(user.Email, callbackUrl);
+
+            try
+            {
+                await _emailService.SendEmailConfirmationAsync(user.Email, callbackUrl);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal server error. Please try again later.");
+            }
+
             return Ok(new { Message = "Registration successful. Please check your email to confirm your account." });
         }
 
 
         [HttpPost("login")]
-        public async Task<IActionResult> LoginAsync([FromBody] LoginRequestModel request)
+        public async Task<IActionResult> LoginAsync([FromBody] LoginCommandModel.LoginCommand request)
         {
             var response = await _mediator.Send(request);
             if (!response.IsSuccessfull)
@@ -132,57 +134,58 @@ namespace Api.Controllers
                 return Unauthorized();
             }
 
-            var token = _authentication.GenerateJWTAuthetication(response.Data.Email, response.Data.RoleId, response.Data.UserId);
+            var getUser = await _user.GetUserAsync(response.Data.UserId);
+           var token = _authentication.GenerateToken(getUser);
             return Ok(new { Token = token });
         }
 
         [HttpGet("confirm-email")]
-        public async Task<IActionResult> ConfirmEmail(string userId, string token)
+        public async Task<IActionResult> ConfirmEmail(Guid userId, string token)
         {
             if (userId == null || token == null)
             {
                 return BadRequest("Invalid email confirmation request.");
             }
 
-            var user = await _userManager.FindByIdAsync(userId);
+            var user = await _user.GetUserAsync(userId);
             if (user == null)
             {
                 return NotFound($"Unable to load user with ID '{userId}'.");
             }
 
-            var result = await _userManager.ConfirmEmailAsync(user, token);
-            if (result.Succeeded)
+            var result = await _tokenService.ValidateEmailTokenAsync(user.Id, token);
+            if (!result)
             {
-                return Ok("Email confirmed successfully.");
+                return BadRequest("Invalid email confirmation request.");
             }
-            return BadRequest("Invalid email confirmation request.");
+                return Ok("Email confirmed successfully.");
         }
 
 
 
         [HttpPost("forgot-password")]
-        public async Task<IActionResult> ForgotPassword(ForgotPasswordRequest model)
+        public async Task<IActionResult> ForgotPassword(PasswordModel.ForgotPasswordRequest model)
         {
-            var user = await _userManager.FindByEmailAsync(model.Email);
+            var user = await _user.GetUserEmailAsync(model.Email);
             if (user == null)
             {
                 return Ok();
             }
 
-            var token = _resetPasswordService.GeneratePasswordResetToken(user);
-            await _userRepo.SavePasswordResetTokenAsync(user, token);
+            var token = _tokenService.GeneratePasswordResetToken(user);
+            await _user.SavePasswordResetTokenAsync(user, token);
 
             var callbackUrl = Url.Action("ResetPassword", "Account", new { token }, Request.Scheme);
             await _emailService.SendPasswordResetEmailAsync(model.Email, callbackUrl);
 
             return Ok();
         }
-
+        
 
         [HttpPost("reset-password")]
-        public async Task<IActionResult> ResetPassword(ResetPasswordRequest model)
+        public async Task<IActionResult> ResetPassword(PasswordModel.ResetPasswordRequest model)
         {
-            var user = await _userRepo.Get(t => t.PasswordResetToken == model.Token);
+            var user = await _user.GetUserTokenAsync(model.Token);
             if (user == null || user.PasswordExpireTime < DateTime.UtcNow)
             {
                 return BadRequest("Invalid or expired token.");
@@ -191,7 +194,7 @@ namespace Api.Controllers
             user.Password = BCrypt.Net.BCrypt.HashPassword( model.Password, user.PasswordResetToken);
             user.PasswordResetToken = null; // Invalidate token
             user.PasswordExpireTime = null;
-            _userRepo.Save();
+            _user.SaveUserAsync();
 
             return Ok();
         }
