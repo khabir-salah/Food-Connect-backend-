e2df91f87cc74de443b549579fd79d1ac4e7a102
Salahudeen Abdulkabir <sabdulkabir19@gmail.com>
2024-07-24 23:44:12 +0100

    command

---
+++ Application/Features/Command/Delete/DeleteDonation.cs
@@ -0,0 +1,31 @@
+﻿using Application.Features.Interfaces.IRepositries;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Command.Delete
+{
+    public class DeleteDonation
+    {
+        public record DeleteDonationCommand(Guid Id) : IRequest;
+        public class DeleteProductRequestHandler : IRequestHandler<DeleteDonationCommand>
+        {
+            private readonly IDonationRepository _donationRepo;
+            public DeleteProductRequestHandler(IDonationRepository donationRepo) 
+            {
+                _donationRepo = donationRepo;
+            }
+            public async Task Handle(DeleteDonationCommand request, CancellationToken cancellationToken)
+            {
+                var getDonation = await _donationRepo.Get(d => d.Id == request.Id);
+                if (getDonation != null)
+                {
+                    _donationRepo.Delete(getDonation);
+                }
+            }
+        }
+    }
+}
---
+++ Application/Features/DTOs/ViewDonationCommandModel.cs
@@ -0,0 +1,43 @@
+﻿using Domain.Enum;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.DTOs
+{
+    public class ViewDonationCommandModel
+    {
+        public record DonationCommand : IRequest<BaseResponse<ICollection<DonationResponseCommandModel>>>
+        {
+            public int DonationType {  get; set; } 
+        }
+
+        public record DonationResponseCommandModel
+        {
+            public string FoodDetails { get; set; } = default!;
+            public int Quantity { get; set; }
+            public DateTime ExpirationDate { get; set; }
+            public DonationStatus Status { get; set; }
+            public DateTime PickUpTime { get; set; }
+            public string PickUpLocation { get; set; } = default!;
+            public IList<IFormFile> DonationImages { get; set; } = null!;
+            public IFormFile PrimaryImageUrl { get; set; } = null!;
+            public UserDonatedResponse UserDetails { get; set; } = null!;
+            public string UserEmail { get; set; }
+            public string UserRole { get; set; }
+        }
+
+        public record UserDonatedResponse
+        {
+            public string UserEmail { get; set; }
+            public string UserRole { get; set; }
+            public string FirstName { get; set; }
+            public string PhoneNumber { get; set; }
+
+        }
+    }
+}
