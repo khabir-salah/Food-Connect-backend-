9200296f753c8a325050568b6fbaad6eacf05571
Salahudeen Abdulkabir <sabdulkabir19@gmail.com>
2024-08-13 23:46:16 +0100

    new method added

---
+++ Application/Features/Interfaces/IServices/IMessageService.cs
@@ -0,0 +1,13 @@
+﻿using Application.Features.DTOs;
+
+
+namespace Application.Features.Interfaces.IServices
+{
+    public interface IMessageService
+    {
+        Task<BaseResponse<MessageCommandModel>> GetMessagesAsync(Guid userId, Guid recipientId);
+        Task<MessageCommandModel> GetMessagesByDonationIdAsync(Guid donationId);
+        Task<bool> MarkAsReceivedAsync(Guid donationId);
+        Task<bool> DeleteMessagesByDonationIdAsync(Guid donationId);
+    }
+}
