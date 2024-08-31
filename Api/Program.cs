5d6b5d88c94885c775e4264a4cff93a597d22951
Salahudeen Abdulkabir <sabdulkabir19@gmail.com>
2024-08-31 07:25:20 +0100

    Dependency added

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,5 +1,5 @@
 
-using Application.Queries;
+using Application.SignalR;
 using Asp.Versioning;
 using Domain.Validations;
 using FluentValidation;
@@ -7,9 +7,11 @@ using FluentValidation.AspNetCore;
 using Infrastructure.Extension;
 using Infrastructure.persistence.Context;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.SignalR;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using static Application.Features.Queries.GeneralServices.Authentication;
 
 namespace Api
 {
@@ -18,64 +20,123 @@ namespace Api
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
             builder.Logging.ClearProviders().AddConsole();
+
             var connectionString = builder.Configuration.GetConnectionString("FoodConnectConnection");
-            // Add services to the container.
+            
+
             builder.Services.AddRepositories();
             builder.Services.AddMediateRs();
+            builder.Services.AddServices();
             builder.Services.AddFluentValidationAutoValidation();
             builder.Services.AddValidatorsFromAssemblyContaining<FamilyValidation>();
-            builder.Services.AddApiVersioning(setup =>
-            {
-                setup.ReportApiVersions = true;
-            }).AddMvc();
+
             builder.Services.AddApiVersioning(setup =>
             {
                 setup.DefaultApiVersion = new ApiVersion(1, 0);
-                setup.AssumeDefaultVersionWhenUnspecified = true; 
+                setup.AssumeDefaultVersionWhenUnspecified = true;
                 setup.ReportApiVersions = true;
-            }).AddMvc();
-            builder.Services.AddControllers();
+            });
+
+            builder.Services.AddControllers().AddFluentValidation();
             builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
-
-
-            builder.Services.AddMvc().AddFluentValidation();
-            builder.Services.AddDbContext<FoodConnectDB>( option => 
-            option.UseMySql( connectionString, ServerVersion.AutoDetect( connectionString ) ) );
-            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            builder.Services.AddHttpContextAccessor();
+            builder.Services.AddSignalR(options =>
             {
-                options.TokenValidationParameters = new TokenValidationParameters
+                options.KeepAliveInterval = TimeSpan.FromMinutes(60*12); 
+                options.ClientTimeoutInterval = TimeSpan.FromMinutes(60*12); 
+            });
+            builder.Services.AddSingleton<IUserIdProvider, CustomUserIdProvider>();
+
+            builder.Services.AddDbContext<FoodConnectDB>(option =>
+                option.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
+            );
+
+            builder.Services.Configure<JWT>(builder.Configuration.GetSection("JWT"));
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(o =>
+            {
+                o.RequireHttpsMetadata = false;
+                o.SaveToken = false;
+                o.TokenValidationParameters = new TokenValidationParameters
                 {
+                    ValidateIssuerSigningKey = true,
                     ValidateIssuer = true,
                     ValidateAudience = true,
                     ValidateLifetime = true,
-                    ValidateIssuerSigningKey = true,
-                    ValidIssuer = builder.Configuration["config:JwtIssuer"],
-                    ValidAudience = builder.Configuration["config:JwtAudience"],
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["config:JwtKey"]))
+                    ClockSkew = TimeSpan.Zero,
+
+                    ValidIssuer = builder.Configuration["JWT:Issuer"],
+                    ValidAudience = builder.Configuration["JWT:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Key"]))
+                };
+
+                o.Events = new JwtBearerEvents
+                {
+                    OnMessageReceived = context =>
+                    {
+                        var accessToken = context.Request.Query["access_token"];
+
+                        var path = context.HttpContext.Request.Path;
+                        if (!string.IsNullOrEmpty(accessToken) &&
+                            (path.StartsWithSegments("/chathub")))
+                        {
+                            context.Token = accessToken;
+                        }
+                        return Task.CompletedTask;
+                    }
                 };
             });
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowSpecificOrigin",
+                    builder => builder
+                        .WithOrigins("https://localhost:7252") 
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowCredentials());
+            });
+
+
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
 
-            app.UseHttpsRedirection();
+            app.UseCors("AllowSpecificOrigin");
 
+            app.UseStaticFiles();
+
+
+
+            app.UseHttpsRedirection();
+            app.UseAuthentication();
             app.UseAuthorization();
 
-
-            app.MapControllers();
+            app.UseRouting();
+            app.UseCors("AllowSpecificOrigin");
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapHub<ChatHub>("/chathub");
+                endpoints.MapHub<NotificationHub>("/notificationHub");
+            });
 
             app.Run();
         }
+
     }
 }
--- Infrastructure/Extension/ServiceCollectionExtension.cs
+++ Infrastructure/Extension/ServiceCollectionExtension.cs
@@ -1,12 +1,12 @@
-﻿using Application.Command;
-using Application.Queries;
-using Microsoft.Extensions.DependencyInjection;
-using Application.Interfaces;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Infrastructure.persistence.Repository.Implementation;
-using Domain.Entities;
-using Microsoft.AspNetCore.Identity;
-using Infrastructure.persistence.Context;
-using Microsoft.AspNet.Identity;
+using Application.Features.Command.Create;
+using Application.Features.Queries.GeneralServices;
+using Application.Features.Queries.Get;
+using Application.Features.Interfaces.IRepositries;
+using Application.Features.Interfaces.IServices;
+using Microsoft.AspNetCore.Http;
+using Application.SignalR;
 
 
 namespace Infrastructure.Extension
@@ -17,57 +17,52 @@ namespace Infrastructure.Extension
         public static IServiceCollection AddRepositories(this IServiceCollection services)
         {
             return services.AddScoped<IDonationRepository, DonationRepository>()
-                .AddScoped<IFamilyRepository, FamilyRepository>()
+                .AddScoped<IFamilyHeadRepository, FamilyHeadRepository>()
             .AddScoped<IFoodCollectionRepository, FoodCollectionRepository>()
                 .AddScoped<IManagerRepository, ManagerRepository>()
                 .AddScoped<IOragnisationRepository, OrganisationRepository>()
-                .AddScoped<IRecipentRepository, RecipentRepository>()
+                .AddScoped<IIndividualRepository, IndividualRepository>()
                 .AddScoped<IRoleRepository, RoleRepository>()
                 .AddScoped<IUserRepository, UserRepository>()
-                .AddScoped<Authentication>()
+                .AddScoped<IMessageRepository, MessageRepository>()
+                .AddScoped<IFamilyRepository, FamilyRepository>()
                 .AddScoped(typeof(GenericRepository<>));
             ;
+        }
+
+        public static void AddServices(this IServiceCollection services)
+        {
+            services.AddScoped<IAuthentication, Authentication>()
+                .AddScoped<ICurrentUser, CurrentUser>()
+                .AddScoped<IUserService, UserService>()
+                .AddScoped<ILogisticsService, LogisticsService>()
+                .AddScoped<ITokenService, TokenService>()
+                .AddScoped<IDonationValidation, DonationValidation>()
+                .AddScoped<IViewProfile, ViewProfile>()
+                .AddScoped<IMessageService, MessageService>()
+                .AddScoped<IDonationFilter, DonationFilter>()
+                .AddTransient<IEmailService, EmailService>()
+                .AddTransient<IDonationReview, DonationReview>()
+                .AddTransient<IDonationService, DonationService>()
+                .AddScoped(typeof(PaginationHelper))
+                .AddSingleton<ConnectionMappingService>()
+            ;
+            services.AddSignalR();
+            services.AddHttpContextAccessor();
+            services.AddSingleton<IUriService>(o =>
+            {
+                var accessor = o.GetRequiredService<IHttpContextAccessor>();
+                var request = accessor.HttpContext.Request;
+                var uri = string.Concat(request.Scheme, "://", request.Host.ToUriComponent());
+                return new UriService(uri);
+            });
         }
 
 
         public static void AddMediateRs(this IServiceCollection services)
         {
-             services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(typeof(CreateFamily).Assembly));
-            services.AddIdentity<User, IdentityRole>()
-                .AddEntityFrameworkStores<FoodConnectDB>()
-        .AddDefaultTokenProviders();
-
-            services.AddIdentity<User, IdentityRole>(options =>
-            {
-                options.Password.RequireDigit = true;
-                options.Password.RequiredLength = 6;
-                options.Password.RequireNonAlphanumeric = false;
-                options.Password.RequireUppercase = true;
-                options.Password.RequireLowercase = true;
-                options.SignIn.RequireConfirmedEmail = true;
-            })
-    .AddDefaultTokenProviders();
-
-            // Manually add EF Core stores
-            services.AddScoped<Microsoft.AspNet.Identity.IUserStore<User>, UserStore<ApplicationUser, IdentityRole, FoodConnectDB>>();
-            services.AddScoped<IRoleStore<IdentityRole>, RoleStore<IdentityRole, FoodConnectDB>>();
-
-
-
-
-
-            services.AddTransient< EmailService>();
-
-            services.Configure<IdentityOptions>(options =>
-            {
-                options.SignIn.RequireConfirmedEmail = true;
-            });
-
-             services.AddScoped<Authentication>().
-                AddScoped<ResetPasswordService>()
-                .AddScoped<UserLogin>();
-
-
+             services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(typeof(CreateFamilyHead).Assembly));
+          
 
         }
 
