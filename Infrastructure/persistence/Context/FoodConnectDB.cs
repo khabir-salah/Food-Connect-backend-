374ae4cd9caff8749588a32e2ad05ffcf080c371
Salahudeen Abdulkabir <sabdulkabir19@gmail.com>
2024-08-31 07:34:07 +0100

    DB modified

--- Infrastructure/persistence/Context/FoodConnectDB.cs
+++ Infrastructure/persistence/Context/FoodConnectDB.cs
@@ -1,4 +1,6 @@
-﻿using Domain.Entities;
+﻿using Application.Features.Interfaces.IRepositries;
+using Domain.Constant;
+using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -11,6 +13,7 @@ namespace Infrastructure.persistence.Context
 {
     public class FoodConnectDB : DbContext
     {
+       
         public FoodConnectDB(DbContextOptions<FoodConnectDB> option) : base(option) { }
 
         public DbSet<Donation> Donation => Set<Donation>();
@@ -25,35 +28,63 @@ namespace Infrastructure.persistence.Context
         public DbSet<Organisation> Organisation => Set<Organisation>();
 
 
-       
 
+       
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+            var password = "1234567";
+            var salt = BCrypt.Net.BCrypt.GenerateSalt(10);
+            var hashPassword = BCrypt.Net.BCrypt.HashPassword(password, salt);
+            modelBuilder.Entity<User>().HasData(
+                new User
+                {
+                    Name = "Salah",
+                    Address = "EVerywhere",
+                    Email = "admin@gmail.com",
+                    IsActivated = true,
+                    IsEmailConfirmed = true,
+                    Password = hashPassword,
+                    PhoneNumber = "1234567890",
+                    RoleId = "00100",
+                    ProfileImage = "fineboy",
+                });
+
             modelBuilder.Entity<Role>().HasData(
                 new Role {
-                    Name = "Manager",
+                    RoleId = "317782478",
+                    Name = RoleConst.Manager,
                     Description = "Manages the system of the application",
                 },
                 new Role {
-                    Name = "FamilyHead", 
+                    RoleId = "5746724647",
+                    Name = RoleConst.FamilyHead, 
                     Description = "Registering on the application as a Head of a family. you will be in charge of all activity on the application on behalf of your Family. \n Pls note that once you register your family they can not register as an individual again"
                 },
-                new Role { 
-                    Name = "OrganisationHead",
+                new Role {
+                    RoleId = "537676327",
+                    Name = RoleConst.OrganizationHead,
                             Description = "Registering on the application as a Head of an Organization. you will be in charge of all activity on the application on behalf of your Organization e.g NGO"
                 },
                 new Role
                 {
+                    RoleId = RoleConst.Individual,
                     Name = "Individual",
                     Description = "Registering on the application as an Individual. \nyou are incharge of every activity on the application"
                 },
                  new Role
                  {
+                     RoleId = "2678834",
                      Name = "Family",
                      Description = "Registering under the Family Head. \nThe Family Head is still incharge of every activity on the application"
-                 }
+                 },
+                  new Role
+                  {
+                      RoleId = "00100",
+                      Name = RoleConst.Admin,
+                      Description = "Baba Alaye"
+                  }
                 ) ;
 
             //modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
